42949252f50fcdd5eef276a3df9de89e66ba3b57
Talha121 <muhammad.ashraff121@gmail.com>
2020-08-30 13:18:54 +0500

    Add project files.

---
+++ Data/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace test1.Data
+{
+   public  class Employee
+    {
+        [Key]
+        public int Employeeid { get; set; }
+        public string Employeename { get; set; }
+        public string Employyephone { get; set; }
+    }
+}
---
+++ Migrations/20200827065618_talha.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace test1.Migrations
+{
+    public partial class talha : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "employees",
+                columns: table => new
+                {
+                    Employeeid = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Employeename = table.Column<string>(nullable: true),
+                    Employyephone = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_employees", x => x.Employeeid);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "employees");
+        }
+    }
+}
